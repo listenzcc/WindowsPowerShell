686e63f08af7cc7dc40d793a5cceecc7714e6f03
listenzcc <cc.public@outlook.com>
2025-09-03 09:28:22 +0800

    Fix formatting in conda initialization block for improved readability

--- profile.ps1
+++ profile.ps1
@@ -6,7 +6,7 @@ Import-Module DirColors
 #region conda initialize
 # !! Contents within this block are managed by 'conda init' !!
 If (Test-Path "C:\Users\liste\miniconda3\Scripts\conda.exe") {
-    (& "C:\Users\liste\miniconda3\Scripts\conda.exe" "shell.powershell" "hook") | Out-String | ?{$_} | Invoke-Expression
+    (& "C:\Users\liste\miniconda3\Scripts\conda.exe" "shell.powershell" "hook") | Out-String | ? { $_ } | Invoke-Expression
 }
 #endregion
 
